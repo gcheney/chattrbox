1303d1259cc12cd436d4c50215a31552d0cf6202
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-22 12:54:57 -0500

    Setup initial connection

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(){
+    console.log('Connected via socket.io');
+});
 
 var PORT = process.env.PORT || 3000;
 http.listen(PORT, function(){
