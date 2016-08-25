eeb9a01ba1c48d9ab4fc6faf63800fefaed15c01
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-25 08:21:16 -0500

    Listen for events in the client

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connection', function(){
     console.log('Connected via socket.io');
 });
+
+socket.on('message', function(message){
+    console.log('New message: ' + message.text);
+});
