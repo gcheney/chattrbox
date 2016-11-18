fb7aa9aaa29ef1801174691537876ac90dcb35e7
Glendon Cheney <glendon.cheney@gmail.com>
2016-11-18 17:23:39 -0600

    Wrap client side code in IIFE

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,55 @@
-var socket = io();
+(function(){
+    var socket = io();
 
-// on connection - join room
-socket.on('connect', function() {
-    // get query string variables
-    var name = getQueryString('name') || 'Guest';
-    var room = getQueryString('room');
-    
-    // dynamically set room name
-    $('.room-name').text(room);
-    
-    socket.emit('joinRoom', {
-        name: name, 
-        room: room
+    // on connection - join room
+    socket.on('connect', function() {
+        // get query string variables
+        var name = getQueryString('name') || 'Guest';
+        var room = getQueryString('room');
+
+        // dynamically set room name
+        $('.room-name').text(room);
+
+        socket.emit('joinRoom', {
+            name: name, 
+            room: room
+        });
     });
-});
 
-// when message is received
-socket.on('message', function(message) {
-    var timestamp = moment.utc(message.timestamp);   
-    var $messageGroup = $('.messages');
-    
-    var $message = $('<li class="list-group-item"></li>');
-    $message.append('<p><strong>' + message.name + ' ' 
-                    + timestamp.local().format('h:mm a') 
-                    +  ': </strong>' + message.text + '</p>');  
-    $messageGroup.append($message);
-    $(window).scrollTo('max'); // scroll to bottom on new message
-});
+    // when message is received
+    socket.on('message', function(message) {
+        var timestamp = moment.utc(message.timestamp);   
+        var $messageGroup = $('.messages');
 
-// create current users list for new visitor
-socket.on('sendCurrentUsers', function(userData) {
-    userData.users.forEach(function(user) {
-        var text = '<p id="' + user + '">' + user + '</p>';
+        var $message = $('<li class="list-group-item"></li>');
+        $message.append('<p><strong>' + message.name + ' ' 
+                        + timestamp.local().format('h:mm a') 
+                        +  ': </strong>' + message.text + '</p>');  
+        $messageGroup.append($message);
+        $(window).scrollTo('max'); // scroll to bottom on new message
+    });
+
+    // create current users list for new visitor
+    socket.on('sendCurrentUsers', function(userData) {
+        userData.users.forEach(function(user) {
+            var text = '<p id="' + user + '">' + user + '</p>';
+            $('.current-users').append(text);
+        }); 
+    });
+
+    // Add new users to current users list
+    socket.on('newUser', function(user) {
+        var text ='<p id="' + user.name + '">'  + user.name + '</p></li>';
         $('.current-users').append(text);
-    }); 
-});
+    });
 
-// Add new users to current users list
-socket.on('newUser', function(user) {
-    var text ='<p id="' + user.name + '">'  + user.name + '</p></li>';
-    $('.current-users').append(text);
-});
+    // Remove user from current users list
+    socket.on('removeUser', function(user) {
+        var target = '#' + user.name;
+        $(target).remove();
+    });
 
-// Remove user from current users list
-socket.on('removeUser', function(user) {
-    var target = '#' + user.name;
-    $(target).remove();
-});
 
-$(document).ready(function(){
     // confirm navigation to new chat room
     $('#change-rooms').on('click', function(e) {
         return confirm('Are you sure you want to leave this chatroom?');
@@ -95,26 +96,24 @@ $(document).ready(function(){
             }
         }
     });
-});
 
-
-// Utility functions
-
-// check for string content
-function isBlankOrEmpty(str) {
-    return (!str || 0 === str.length || /^\s*$/.test(str));
-}
-
-// parse query strings
-function getQueryString(str) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == str) {
-            return decodeURIComponent(pair[1].replace(/\+/g, ' '));
-        }
+    // Utility functions
+    // check for string content
+    function isBlankOrEmpty(str) {
+        return (!str || 0 === str.length || /^\s*$/.test(str));
     }
-    
-    return undefined;
-}
+
+    // parse query strings
+    function getQueryString(str) {
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split('=');
+            if (decodeURIComponent(pair[0]) == str) {
+                return decodeURIComponent(pair[1].replace(/\+/g, ' '));
+            }
+        }
+
+        return undefined;
+    }
+})();
